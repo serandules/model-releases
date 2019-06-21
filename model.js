1ecfd3311968676a3e7cc642181726f5dd35612a
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-06-21 14:32:49 +0530

    updating

--- model.js
+++ model.js
@@ -2,11 +2,11 @@ var log = require('logger')('model-releases');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var release = Schema({
+var schema = Schema({
   type: {type: String, required: true, enum: ['serandules', 'serandomps']},
   name: {type: String, required: true},
   version: {type: String, required: true},
   revision: String
 }, {collection: 'releases'});
 
-module.exports = mongoose.model('releases', release);
+module.exports = mongoose.model('releases', schema);
