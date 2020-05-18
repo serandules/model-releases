38e710c6152a73af41bc8e078cc861a7edbb12bd
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2020-05-18 21:07:30 +0530

    updating

--- model.js
+++ model.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = Schema({
-  type: {type: String, required: true, enum: ['serandules', 'serandomps']},
-  name: {type: String, required: true},
-  version: {type: String, required: true},
+  type: {type: String, required: true, index: true, enum: ['serandules', 'serandomps']},
+  name: {type: String, required: true, index: true},
+  version: {type: String, required: true, index: true},
   revision: String
 }, {collection: 'releases'});
 
